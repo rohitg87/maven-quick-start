82dd16c9ee1f3b48989b8d3d63a915ccc84e45f2
Rohit Gunjal <rohitg87@gmail.com>
2022-10-11 19:03:49 -0400

    Adding common lang3 dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,9 +2,16 @@ package clinic.programming.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
-	public void greeting(){
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length ;
+	}
+	
+	public void greet(){
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
 		
@@ -21,6 +28,9 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
-		app.greeting();
+		app.greet();
+		
+		int count = app.countWords("I have four words");
+		System.out.println("Word count: "+ count);
     }
 }
